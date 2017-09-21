ab479dc5219fe0023148464d06147bb389e9d31d
Stefan Hausotte <stefan.hausotte@gdata.de>
2017-09-21 10:33:34 +0200

    Added .Net Framework console application

---
+++ NetFrameworkConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace NetFrameworkConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.EnumerateDirectories(@"C:\");
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+        }
+    }
+}
